933c87c2e768abec087cf6011e2c603da97cef1b
BolleTito335409 <chemichaeldekraker@gmail.com>
2023-02-02 20:28:20 +0100

    create aangepast

--- create.php
+++ create.php
@@ -14,37 +14,32 @@ try {
     echo "Systeemmelding: " . $e->getMessage();
 }
 // Maak de sql query voor het inserten van een record
-$sql = "INSERT INTO Persoon (Id
-                            ,Voornaam
-                            ,Tussenvoegsel
-                            ,Achternaam
-                            ,Telefoonnummer
-                            ,Straatnaam
-                            ,Huisnummer
-                            ,Woonplaats
-                            ,Postcode
-                            ,Landnaam)
+$sql = "INSERT INTO Pizza (Id
+                            ,Bodemformaat
+                            ,Saus
+                            ,Pizzatoppings
+                            ,Peterselie
+                            ,Oregano
+                            ,Chiliflakes
+                            ,Zwartepeper)
         VALUES              (NULL
-                            ,:firstname
-                            ,:infix
-                            ,:lastname
-                            ,:phonenumber
-                            ,:streetname
-                            ,:housenumber
-                            ,:residence
-                            ,:postalcode
-                            ,:country);";
+                            ,:bodem
+                            ,:saus
+                            ,:pizzatoppings
+                            ,:peterselie
+                            ,:oregano
+                            ,:chiliflakes
+                            ,:zwartepeper);";
 // Maak de query gereed met de prepare-method van het $pdo-object
 $statement = $pdo->prepare($sql);
-$statement->bindValue(':firstname', $_POST['firstname'], PDO::PARAM_STR);
-$statement->bindValue(':infix', $_POST['infix'], PDO::PARAM_STR);
-$statement->bindValue(':lastname', $_POST['lastname'], PDO::PARAM_STR);
-$statement->bindValue(':phonenumber', $_POST['phonenumber'], PDO::PARAM_STR);
-$statement->bindValue(':streetname', $_POST['streetname'], PDO::PARAM_STR);
-$statement->bindValue(':housenumber', $_POST['housenumber'], PDO::PARAM_STR);
-$statement->bindValue(':residence', $_POST['residence'], PDO::PARAM_STR);
-$statement->bindValue(':postalcode', $_POST['postalcode'], PDO::PARAM_STR);
-$statement->bindValue(':country', $_POST['country'], PDO::PARAM_STR);
+$statement->bindValue(':bodem', $_POST['bodem'], PDO::PARAM_STR);
+$statement->bindValue(':saus', $_POST['saus'], PDO::PARAM_STR);
+$statement->bindValue(':pizzatoppings', $_POST['pizzatoppings'], PDO::PARAM_STR);
+$statement->bindValue(':peterselie', $_POST['kruiden'], PDO::PARAM_STR);
+$statement->bindValue(':oregano', $_POST['kruiden'], PDO::PARAM_STR);
+$statement->bindValue(':chiliflakes', $_POST['kruiden'], PDO::PARAM_STR);
+$statement->bindValue(':zwartepeper', $_POST['kruiden'], PDO::PARAM_STR);
+
 // Vuur de query af op de database...
 $statement->execute();
 
